ac74cfe841c076f93f80c2315ce8d70175195809
faynald <72617106+faynald@users.noreply.github.com>
2020-12-30 20:34:40 +0700

    updated action bar login

--- app/src/main/java/com/pemvis/maskerku/LoginActivity.java
+++ app/src/main/java/com/pemvis/maskerku/LoginActivity.java
@@ -9,6 +9,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException ignored){}
         setContentView(R.layout.activity_login);
     }
 }
