376bd2cbe2537ea91ec6866295e66267c78db2cd
faynald <72617106+faynald@users.noreply.github.com>
2020-12-31 18:39:38 +0700

    removed some unused codes

--- app/src/main/java/com/pemvis/maskerku/CreateYourMaskActivity.java
+++ app/src/main/java/com/pemvis/maskerku/CreateYourMaskActivity.java
@@ -1,13 +1,10 @@
 package com.pemvis.maskerku;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
 public class CreateYourMaskActivity extends AppCompatActivity {
     @Override
