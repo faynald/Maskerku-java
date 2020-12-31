efe85f36a672618e425ecef60a75b107fb20ff3a
faynald <72617106+faynald@users.noreply.github.com>
2020-12-31 11:22:12 +0700

    created navigation drawer

--- app/src/main/java/com/pemvis/maskerku/MainActivity.java
+++ app/src/main/java/com/pemvis/maskerku/MainActivity.java
@@ -1,22 +1,40 @@
 package com.pemvis.maskerku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void toLogin(View v){
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
     }
     public void toCreateYourMask(View v){
         startActivity(new Intent(MainActivity.this, create_your_mask.class));
     }
+
+    public void toLogin(MenuItem item) {
+        drawerLayout.closeDrawers();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
 }
