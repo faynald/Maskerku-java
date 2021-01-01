8efe3de8104ebc015cb02ca95d43f8e3c301074e
faynald <72617106+faynald@users.noreply.github.com>
2021-01-01 11:29:50 +0700

    added terms and conditions pop up

--- app/src/main/java/com/pemvis/maskerku/CreateYourMaskActivity.java
+++ app/src/main/java/com/pemvis/maskerku/CreateYourMaskActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CreateYourMaskActivity extends AppCompatActivity {
+    Dialog dialogMaskerTerms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +24,22 @@ public class CreateYourMaskActivity extends AppCompatActivity {
 
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        dialogMaskerTerms = new Dialog(this);
+    }
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        Button buttonMengerti;
+        dialogMaskerTerms.setContentView(R.layout.custom_popup_terms);
+        txtclose =(TextView) dialogMaskerTerms.findViewById(R.id.txtclose);
+        buttonMengerti = (Button) dialogMaskerTerms.findViewById(R.id.buttonMengerti);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogMaskerTerms.dismiss();
+            }
+        });
+        dialogMaskerTerms.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialogMaskerTerms.show();
     }
 }
