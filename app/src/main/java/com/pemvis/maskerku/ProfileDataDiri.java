e8623f12e543ae0014fdacd22c0f352aaaebcd61
faynald <72617106+faynald@users.noreply.github.com>
2021-01-01 20:29:37 +0700

    added profile data diri page

---
+++ app/src/main/java/com/pemvis/maskerku/ProfileDataDiri.java
@@ -0,0 +1,22 @@
+package com.pemvis.maskerku;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class ProfileDataDiri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_data_diri);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
