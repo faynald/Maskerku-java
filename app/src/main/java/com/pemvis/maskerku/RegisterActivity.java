7dafed7bbf97f01548906b946883e29b27c59c72
faynald <72617106+faynald@users.noreply.github.com>
2021-01-02 13:19:34 +0700

    added intent to login page from register page

--- app/src/main/java/com/pemvis/maskerku/RegisterActivity.java
+++ app/src/main/java/com/pemvis/maskerku/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.pemvis.maskerku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void toLogin(View v){
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
