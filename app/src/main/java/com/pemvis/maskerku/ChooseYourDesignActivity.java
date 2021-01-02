d9c1d3c49e474e465b8077d34ab8b55e7e64f21e
faynald <72617106+faynald@users.noreply.github.com>
2021-01-02 22:10:06 +0700

    added back arrow to choose page's toolbar

--- app/src/main/java/com/pemvis/maskerku/ChooseYourDesignActivity.java
+++ app/src/main/java/com/pemvis/maskerku/ChooseYourDesignActivity.java
@@ -1,8 +1,10 @@
 package com.pemvis.maskerku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseYourDesignActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class ChooseYourDesignActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_your_design);
 
-
+        Toolbar toolbar = findViewById(R.id.toolbar_choose_your_design);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_white_24);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
