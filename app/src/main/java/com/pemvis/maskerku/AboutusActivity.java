d94775964102ce869ef41cfde533ca9304d14ce6
faynald <72617106+faynald@users.noreply.github.com>
2021-01-06 23:48:20 +0700

    added toolbar to about us page

--- app/src/main/java/com/pemvis/maskerku/AboutusActivity.java
+++ app/src/main/java/com/pemvis/maskerku/AboutusActivity.java
@@ -1,8 +1,10 @@
 package com.pemvis.maskerku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutusActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class AboutusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_setting);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_white_24);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
