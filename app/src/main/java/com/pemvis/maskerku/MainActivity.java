247c5210ae868bcc314f58df261c97a5cd01e629
faynald <72617106+faynald@users.noreply.github.com>
2021-01-20 20:44:29 +0700

    UI improvements

--- app/src/main/java/com/pemvis/maskerku/MainActivity.java
+++ app/src/main/java/com/pemvis/maskerku/MainActivity.java
@@ -7,10 +7,17 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -21,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private long backPressedTime;
     private Toast backToast;
+    private ImageView imageMasker;
+    Dialog dialogMasker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
         }
 
+        dialogMasker = new Dialog(this);
     }
     public void toCreateYourMask(View v){
         startActivity(new Intent(MainActivity.this, CreateYourMaskActivity.class));
@@ -81,6 +91,30 @@ public class MainActivity extends AppCompatActivity {
     public void toLogin(MenuItem item) {
         drawerLayout.closeDrawers();
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void popupTerpopuler1(View v){
+        dialogMasker.show();
+        TextView x_button;
+        Button button_ok;
+        dialogMasker.setContentView(R.layout.custom_popup_masker);
+        x_button = (TextView) dialogMasker.findViewById(R.id.x_button);
+        button_ok = (Button) dialogMasker.findViewById(R.id.button_ok);
+        imageMasker = (ImageView) findViewById(R.id.popup_masker_image);
+        // error: imageMasker.setImageResource(R.drawable.terpopuler_1);
+        x_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogMasker.dismiss();
+            }
+        });
+        button_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogMasker.dismiss();
+            }
+        });
+
     }
 
     @Override
